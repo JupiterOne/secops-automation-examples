5a45acdc3d9a3794288f9dedac318857b2e2ad18
Erich Smith <erich.smith@jupiterone.com>
2021-09-16 10:56:06 -0400

    Initial take at relationship creation

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,35 @@ require("dotenv").config();
     account: process.env.J1_ACCOUNT,
   });
 
-  const result = await j1Client.queryV1(`
+  const results = await j1Client.queryV1(`
   Find (Function|Task) with displayName = 'jupiter-query-service' as f1 
   THAT ASSIGNED AccessRole 
   THAT ASSIGNED AccessPolicy 
   THAT ALLOWS (Function|Task|Database) as f2
   RETURN 
-    f1.displayName as sourceName, f1._id as sourceId, f1._class as sourceClass, f1._type as sourceType,
-    f2.displayName as sinkName, f2._id as sinkId, f2._class as sinkClass, f2._type as sinkType`);
+    f1.displayName as sourceName, f1._id as sourceId, f1._class as sourceClass, f1._type as sourceType, f1._key as sourceKey,
+    f2.displayName as sinkName, f2._id as sinkId, f2._class as sinkClass, f2._type as sinkType, f2._key as sinkKey`);
 
-  console.log(result);
+  console.log(results);
+
+  for (const result of results) {
+    const { sourceId, sourceKey, sourceType, sourceClass, sourceName, sinkId, sinkKey, sinkType, sinkClass, sinkName } = result;
+    const relVerb = sinkClass === 'Database' ? 'ACCESSES' : 'EXECUTES';
+    const relationshipKey = sourceKey + '|' + relVerb.toLowerCase() + '|' + sinkKey;
+    const relationshipType = sourceType + '_' + relVerb.toLowerCase() + '_' + sinkType;
+    const relationship = await j1Client.createRelationship(
+      relationshipKey,
+      relationshipType,
+      relVerb,
+      sourceId,
+      sinkId,
+      { 
+        pseudoRelationship: true,
+        hackathon2021: true 
+      }
+    );
+  }
+ 
 })().catch((err) => {
   console.error("", err);
 });
