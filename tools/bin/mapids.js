9b18eab5f9ca045f2f7b2a922c7b006750eac20c
Erich Smith <erich.smith@jupiterone.com>
2021-09-16 18:04:24 -0400

    Add bulk-delete script for errant relationships

---
+++ tools/bin/mapids.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const data = JSON.parse(fs.readFileSync('./results.json', 'utf8'));
+
+const modified = { 
+  relationships: data.map(i => {
+    return { 
+      relationship: {
+        _id: i._id
+      }
+    };
+  })
+};
+fs.writeFileSync('bulkDelete.json', JSON.stringify(modified, null, 2));
