385a1cd9c9c98a9c1cbf0baa30ab4cfaff31363c
Erich Smith <erich.smith@jupiterone.com>
2021-09-17 09:49:09 -0400

    Minor fix for mapids

--- tools/bin/mapids.js
+++ tools/bin/mapids.js
@@ -1,13 +1,11 @@
 const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('./results.json', 'utf8'));
 
-const modified = { 
-  relationships: data.map(i => {
-    return { 
-      relationship: {
-        _id: i._id
-      }
-    };
-  })
-};
+const modified = data.map(i => {
+  return { 
+    relationship: {
+      _id: i._id
+    }
+  };
+});
 fs.writeFileSync('bulkDelete.json', JSON.stringify(modified, null, 2));
